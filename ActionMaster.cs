901209fb04638d5198d5947018b519f83f9b6cfb
Rishabh090 <rishchelsea@gmail.com>
2018-03-03 02:03:56 +0530

    initial commit

---
+++ ActionMaster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ActionMaster {
+	public enum Action {Tidy, Reset, EndTurn, EndGame, Undefined};
+	
+	public static Action NextAction (List<int> rolls) {
+		Action nextAction = Action.Undefined;
+		
+		for (int i = 0; i < rolls.Count; i++) { 
+			
+			if (i == 20) {
+				nextAction = Action.EndGame;
+			} else if ( i >= 18 && rolls[i] == 10 ){ 
+				nextAction = Action.Reset;
+			} else if ( i == 19 ) {
+				if (rolls[18]==10 && rolls[19]==0) {
+					nextAction = Action.Tidy;
+				} else if (rolls[18] + rolls[19] == 10) {
+					nextAction = Action.Reset;
+				} else if (rolls [18] + rolls[19] >= 10) {  
+					nextAction = Action.Tidy;
+				} else {
+					nextAction = Action.EndGame;
+				}
+			} else if (i % 2 == 0) {
+				if (rolls[i] == 10) {
+					rolls.Insert (i, 0); 
+					nextAction = Action.EndTurn;
+				} else {
+					nextAction = Action.Tidy;
+				}
+			} else {
+				nextAction = Action.EndTurn;
+			}
+		}
+		
+		return nextAction;
+	}
+}
---
+++ PinCounter.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinCounter : MonoBehaviour {
+	public Text standingDisplay;
+
+	private GameManager gameManager;
+	private bool ballOutOfPlay = false;
+	private int lastStandingCount = -1;
+	private int lastSettledCount = 10;
+	private float lastChangeTime;
+
+	void Start () {
+		gameManager = GameObject.FindObjectOfType<GameManager> ();
+	}
+	
+	void Update () {
+		standingDisplay.text = CountStanding ().ToString ();
+		
+		if (ballOutOfPlay) {
+			UpdateStandingCountAndSettle();
+			standingDisplay.color = Color.red;
+		}
+	}
+
+	public void Reset () {
+		lastSettledCount = 10;
+	}
+
+	void OnTriggerExit (Collider collider) {
+		if (collider.gameObject.name == "Ball") {
+			ballOutOfPlay = true;
+		}
+	}
+
+	void UpdateStandingCountAndSettle () {
+		int currentStanding = CountStanding ();
+		
+		if (currentStanding != lastStandingCount) {
+			lastChangeTime = Time.time;
+			lastStandingCount = currentStanding;
+			return;
+		}
+		
+		float settleTime = 3f; 
+		if ((Time.time - lastChangeTime) > settleTime) { 
+			PinsHaveSettled();
+		}
+		
+	}
+	
+	void PinsHaveSettled () {
+		int standing = CountStanding ();
+		int pinFall = lastSettledCount - standing;
+		lastSettledCount = standing;
+
+		gameManager.Bowl (pinFall);
+
+		lastStandingCount = -1; 
+		ballOutOfPlay = false;
+		standingDisplay.color = Color.green;
+	}
+	
+	int CountStanding () {
+		int standing = 0;
+		
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
+			if (pin.IsStanding()) {
+				standing++;
+			}
+		}
+		
+		return standing;
+	}
+}
